c7a1c15ea3cf80ca51ffe5abda036aec13a8ce7f
Dean Martin García <dean.martin@gracia.lasalle.cat>
2025-12-01 23:21:14 +0100

    Hacemos todos los bloques del primer jugador

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
-let player = 0
+let segundo_player = 0
 input.onButtonPressed(Button.A, function () {
-    player = 1
+    segundo_player = 1
+    basic.showIcon(IconNames.SmallSquare)
+})
+input.onButtonPressed(Button.AB, function () {
+    segundo_player = 3
     basic.showIcon(IconNames.Scissors)
+})
+input.onButtonPressed(Button.B, function () {
+    segundo_player = 2
+    basic.showIcon(IconNames.Square)
 })
 basic.forever(function () {
 	
